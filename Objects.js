2aece6a817b3ee291dee64f31426745af9154487
TylerEbner <30875263+TylerEbner@users.noreply.github.com>
2017-08-28 08:47:21 -1000

    Update Objects.js

--- Objects.js
+++ Objects.js
@@ -5,3 +5,15 @@ age: 75,
 };
 
 window.alert(pres.fName)
+
+/*August 28. Replacing objects in an object name*/
+
+var newName = prompt("What is his new first name?");
+//I am taking a users prompt//
+var students = {
+	firstName:"Cole",
+  lastName:"House",
+  age: 15,
+  grade: 10
+}
+window.alert(cole.firstName = newName);
