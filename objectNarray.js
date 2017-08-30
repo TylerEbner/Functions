83709b46646ba6405aec5e298611b01d1dca7c57
TylerEbner <30875263+TylerEbner@users.noreply.github.com>
2017-08-30 08:24:14 -1000

    I made objects and arrays.

---
+++ objectNarray.js
@@ -0,0 +1,27 @@
+  var patty = {
+  	fName: "Patty",
+    age: 13,
+    grade: 9,
+    friends: ["Brad", "Bob", "Bill"]
+    
+  };
+  
+ 	var john = {
+  	fName: "John",
+    age: 14,
+    grade: 10,
+    friends: ["Ty", "Dylan", "Billy"]
+   };
+   
+	var caleb = {
+  	fName: "Patty",
+    age: 15,
+    grade: 11,
+    friends: ["Ray", "Willy", "Richard"]
+   };
+   
+   var x =[patty, john, caleb];
+   
+   window.alert(x[1].friends[0]);
+   
+   
